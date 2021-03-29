b877649bd40cf61e294e29c25698f6cfaf24e5ce
gsk0210 <gargi.sanadhya@knoldus.com>
2021-03-30 00:01:08 +0530

    Committed

--- src/main/java/com/knoldus/springbootapplication/CurrentTimeController.java
+++ src/main/java/com/knoldus/springbootapplication/CurrentTimeController.java
@@ -10,17 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
  * class CurrentTimeController.
  */
 @RestController
-public class CurrentTimeController {
-    /**
-     * Object dateTimeFormatter of class DateTimeFormatter
-     * Object currentTime of class LocalTime
-     */
+public class CurrentTimeController  {
+  /**
+   * Method called at /status api.
+   *
+   * @return String
+   */
+  @GetMapping("/status")
+  public String currentTimeMethod() {
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter
             .ofPattern("dd/MM/yyyy HH:mm:ss");
     LocalDateTime currentTime = LocalDateTime.now();
-    @GetMapping("/status")
-    public String currentTime() {
-        return "Running { " + dateTimeFormatter.format(currentTime) + " }";
-    }
-
+    return "Running { " + dateTimeFormatter.format(currentTime) + " }";
+  }
 }
+
+
